531fc57c7630cab85c1b3e441733faf88f0c274f
Alex Rodrigues <alex.rodrigues.developer@gmail.com>
2018-11-09 17:01:00 -0200

    we came to make a bang

--- app/src/main/java/br/com/rodriguesalex/cast/MainActivity.kt
+++ app/src/main/java/br/com/rodriguesalex/cast/MainActivity.kt
@@ -1,36 +1,33 @@
 package br.com.rodriguesalex.cast
 
+import android.graphics.drawable.AnimationDrawable
 import android.net.Uri
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import android.util.DisplayMetrics
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.SeekBar
-import com.bumptech.glide.Glide
-
-import kotlinx.android.synthetic.main.activity_main.*
-import com.google.android.exoplayer2.source.ExtractorMediaSource
-import com.google.android.exoplayer2.ExoPlayerFactory
-import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
-import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
-import com.google.android.exoplayer2.util.Util
-import kotlinx.android.synthetic.main.content_main.*
-import com.google.android.exoplayer2.upstream.RawResourceDataSource
-import com.google.android.exoplayer2.source.MediaSource
 import android.view.animation.TranslateAnimation
-import android.util.DisplayMetrics
-import android.widget.RelativeLayout
-
-
-
-
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import android.support.v4.media.session.MediaControllerCompat.setMediaController
+import android.view.View
+import android.widget.MediaController
+import android.widget.VideoView
+import com.bumptech.glide.Glide
 
 
 class MainActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
+
+    private val formationSparkles = intArrayOf(R.drawable.formacao_1, R.drawable.formacao_2,
+                        R.drawable.formacao_3, R.drawable.formacao_4, R.drawable.formacao_5,
+            R.drawable.formacao_6, R.drawable.formacao_7, R.drawable.formacao_8, R.drawable.formacao_9,
+            R.drawable.formacao_10, R.drawable.formacao_11, R.drawable.formacao_12, R.drawable.formacao_13,
+            R.drawable.formacao_14, R.drawable.formacao_15, R.drawable.formacao_16, R.drawable.formacao_17,
+            R.drawable.formacao_18, R.drawable.formacao_20, R.drawable.formacao_21, R.drawable.formacao_22,
+            R.drawable.formacao_23, R.drawable.formacao_24, R.drawable.formacao_25, R.drawable.formacao_26)
 
     override fun onStartTrackingTouch(p0: SeekBar?) {
     }
@@ -44,7 +41,6 @@ class MainActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
     }
 
     override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,10 +49,24 @@ class MainActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            moveToCenter()
+            playSparkleInFormation()
+
+            Handler().postDelayed({
+                runOnUiThread {
+                    dismissFirstMoment()
+                    showSecondMoment()
+                }
+            }, 3000)
         }
+    }
 
+    private fun dismissFirstMoment() {
+        rlFirstMoment.animate().alpha(0f)
+    }
 
+    private fun showSecondMoment() {
+        rlSecondMoment.animate().alpha(1f)
+        moveToCenter()
     }
 
     private fun moveToCenter() {
@@ -67,17 +77,36 @@ class MainActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
         val statusBarOffset = dm.heightPixels - root.measuredHeight
 
         val originalPos = IntArray(2)
-        ivCoverSecundary.getLocationOnScreen(originalPos)
+        rlSecondMoment.getLocationOnScreen(originalPos)
 
         var xDest = dm.widthPixels / 2
         xDest -= vReferencia.measuredWidth / 2
-        val yDest = dm.heightPixels / 2 - (vReferencia.getMeasuredHeight() / 2 - statusBarOffset)
+        val yDest = dm.heightPixels / 2 - (vReferencia.measuredHeight / 2 - statusBarOffset)
 
         val anim = TranslateAnimation(0f, (xDest - originalPos[0]).toFloat(), 0f, (yDest - originalPos[1]).toFloat())
         anim.duration = 1000
         anim.fillAfter = true
-        ivCoverSecundary.startAnimation(anim)
+        rlSecondMoment.startAnimation(anim)
+        (ivSparkle.background as AnimationDrawable).start()
     }
+
+
+    private fun playSparkleInFormation() {
+        (ivSparkle1.background as AnimationDrawable).start()
+    }
+
+    /*
+    private fun playVideo() {
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+        videoView.setMediaController(mediaController)
+        videoView.keepScreenOn = true
+        videoView.setVideoPath("android.resource://" + packageName + "/" + R.raw.formacao)
+        videoView.start()
+        videoView.requestFocus()
+    }
+
+    */
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
